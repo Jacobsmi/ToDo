9f9bc14eb98eb6f60f7d319a164e16f997fa014b
Michael Jacobs <jacobsm84@gmail.com>
2021-10-31 18:58:48 -0400

    Board Display Added with basic functionality

--- client/src/components/boardDisplay/boardDisplay.tsx
+++ client/src/components/boardDisplay/boardDisplay.tsx
@@ -1,14 +1,44 @@
+import { Modal, Typography, TextField, ButtonBase } from "@mui/material";
+
 interface BoardDisplayProps {
-    activeBoardID: number;
-    addBoardModalOpen: boolean;
+  activeBoardID: number;
+  addBoardModalOpen: boolean;
+  setAddBoardModalOpen(value: boolean): void;
 }
 
-const BoardDisplay = ({activeBoardID, addBoardModalOpen}:BoardDisplayProps) =>{
-    return(
-        <div className="BoardDisplay">
-            {activeBoardID}
+const BoardDisplay = ({
+  activeBoardID,
+  addBoardModalOpen,
+  setAddBoardModalOpen,
+}: BoardDisplayProps) => {
+  return (
+    <div className="BoardDisplay">
+      {activeBoardID}
+      <Modal
+        open={addBoardModalOpen}
+        onClose={() => {
+          setAddBoardModalOpen(false);
+        }}
+        style={{display:'flex',alignItems:'center',justifyContent:'center'}}
+      >
+        <div
+            style={{
+                height: "50%",
+                width: "50%",
+                backgroundColor: "white",
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+                justifyContent: "space-around"
+            }}
+        >
+            <Typography variant="h5" align="center" style={{marginTop: "2%"}}>Add a Board</Typography>
+            <TextField label="Board Name" ></TextField>
+            <ButtonBase style={{backgroundColor: "#8F8F8F", padding: "10px", borderRadius: "10px"}}>Add Board</ButtonBase>
         </div>
-    )
-}
+      </Modal>
+    </div>
+  );
+};
 
 export default BoardDisplay;
